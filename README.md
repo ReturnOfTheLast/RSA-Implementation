3f6cf4c3523a35fddc783dd0c7225f1d5d0934e1
ReturnOfTheLast <returnofthelast@gmail.com>
2020-11-25 11:30:17 +0100

    Keys are now written to files

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # RSA-Implementation
-A implementation of RSA for my SOP
 
+A implementation of RSA for my SOP
 
 This implementation should not be seen as secure in anyway.
 
 ## Installation
+
 Install [python3](https://python.org/downloads/)
 
-
 Download and unzip the primenumber list from [here](https://lyngbyekolbe.dk/RSA_Implementation/primenums.zip) in the root of the project
+
+## Usage
+
+Run **keygen.py** to generate your keypair:
+
+```bash
+python keygen.py -b <keysize>
+```
+
+You will get two files in the **generated_keys** directory. The files is named in the format
+
+```text
+<year>-<month>-<day>_<hours><minutes><seconds>.<priv|pub>.json
+```
+
+The first extension declares if the key is private or public. You can share the public with others, but keep the private secret.
--- keygen.py
+++ keygen.py
@@ -1,31 +1,37 @@
 import random as __rnd
 import math as __math
 import primegenerator as __pg
+import json as __json
+from datetime import datetime as __dt
 
 def GenerateKeyPair(keysize):
     print("Generating p, q and n")
     n, p, q = __Internal_GenerateN(keysize)
-    print("p = " + str(p) + " (" + str(p.bit_length()) + " bits)")
-    print("q = " + str(q) + " (" + str(q.bit_length()) + " bits)")
-    print("n = " + str(n) + " (" + str(n.bit_length()) + " bits)")
+    print("p = " + hex(p) + " (" + str(p.bit_length()) + " bits)")
+    print("q = " + hex(q) + " (" + str(q.bit_length()) + " bits)")
+    print("n = " + hex(n) + " (" + str(n.bit_length()) + " bits)")
     
     print("")
     
     print("Calculating λ(n) = lcm(p - 1, q - 1)")
     tot = __Internal_FindLeastCommonMultiple(p - 1, q - 1)
-    print("λ(n) = " + str(tot) + " (" + str(tot.bit_length()) + " bits)")
+    print("λ(n) = " + hex(tot) + " (" + str(tot.bit_length()) + " bits)")
 
     print("")
 
     print("Generating public key")
     e = __Internal_GeneratePublicKey(tot, keysize)
-    print("e = " + str(e) + " (" + str(e.bit_length()) + " bits)")
+    print("e = " + hex(e) + " (" + str(e.bit_length()) + " bits)")
 
     print("")
 
     print("Generating private key")
     d = __Internal_GeneratePrivateKey(e, tot)
-    print("d = " + str(d) + " (" + str(d.bit_length()) + " bits)")
+    print("d = " + hex(d) + " (" + str(d.bit_length()) + " bits)")
+
+    print("")
+
+    return (n, p, q, tot, e, d)
 
 def __Internal_GenerateN(keysize):
     
@@ -67,4 +73,31 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="RSA keygen")
     parser.add_argument('--bits', '-b', dest='keysize', type=int, default=1024)
     args = parser.parse_args()
-    GenerateKeyPair(args.keysize)
+    keyParameters = GenerateKeyPair(args.keysize)
+
+    print("Writing keys")
+    keydata = {
+        "priv": {
+            "primes": [
+                keyParameters[1],
+                keyParameters[2]
+            ],
+            "tot": keyParameters[3],
+            "d": keyParameters[5]
+        },
+        "pub": {
+            "n": keyParameters[0],
+            "e": keyParameters[4]
+        }
+    }
+
+    exportdir = "generated_keys"
+    keyprefix = __dt.now().strftime("%Y-%m-%d_%H%M%S")
+
+    with open( exportdir + "/" + keyprefix + ".priv.json", "w") as private_key_file:
+        __json.dump(keydata, private_key_file, indent=2)
+    
+    with open("generated_keys/" + keyprefix + ".pub.json", "w") as public_key_file:
+        __json.dump(keydata["pub"], public_key_file, indent=2)
+
+    print("Keys written to " + exportdir + "/" + keyprefix + ".priv.json and "  + exportdir + "/" + keyprefix + ".pub.json" )
